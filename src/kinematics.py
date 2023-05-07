92ae88263214745d6c9e8f116aa8a91456c90257
Ruslan <ru.abdullin@innopolis.university>
2023-05-07 17:21:47 +0300

    как же меня это заебало

--- src/kinematics.py
+++ src/kinematics.py
@@ -9,12 +9,11 @@ class Kinematics:
     def __init__(self, links: list[Link], dh_table) -> None:
         thetas = list(dynamicsymbols('theta0:6'))
         ee_frame = ee_transformation(thetas, dh_table)
-        self.forward = sp.lambdify(thetas, ee_frame[:3, 3])
-        self.euler = sp.lambdify(thetas, rot2eul(ee_frame[:3, :3]))
+        self.ee_frame = sp.lambdify(thetas, ee_frame)
 
-        transformations = list(dynamicsymbols('px py pz alpha beta gamma'))
-        target_frame = target_transformation(transformations)
-        self.target = sp.lambdify(transformations, rot2eul(ee_frame[:3, :3]))
+        target = list(dynamicsymbols('px py pz alpha beta gamma'))
+        target_frame = target_transformation(target)
+        self.target_frame = sp.lambdify(target, target_frame)
 
         self.dh_table = dh_table
         self.links = links
@@ -23,61 +22,18 @@ class Kinematics:
         if target is None:
             target = self.get_links_position()
 
-        target = np.array([sp.rad(theta + tar)
-                           for tar, theta in zip(target, self.dh_table['theta'])],
-                          dtype=float)
+        target = np.array([sp.rad(tar) for tar in target], dtype=float)
 
-        position = self.forward(*target).tolist()
-        orientation = self.euler(*target).tolist()
+        ee_frame = self.ee_frame(*target)
+        position = ee_frame[:3, 3].tolist()
+        orientation = rot2eul(ee_frame[:3, :3]).tolist()
 
         return [element[0] for element in (position + orientation)]
 
+    def inverse_kinematics(self, target, dt=0.001, kp=10, max_dist=0.1):
+
+
+        return []
+
     def get_links_position(self) -> list[float]:
         return [link.get_position() for link in self.links]
-
-    def inverse_kinematics(self, target, dt=0.001, kp=10, max_dist=0.1):
-        qi = np.array(self.get_links_position(), dtype=float)
-        ee_frame = self.forward_kinematics(qi)
-
-        target_frame = self.target(target)
-        desire_position = target_frame[:3, 3]
-        desire_orientation = target_frame[:3, 0]
-
-        q = 1 - desire_orientation.dot(ee_frame[:3, 0])
-
-        j_position = jacobian_matrix(ee_frame[:3, 3], thetas)
-        j_orientation = desire_orientation.T * (jacobian_matrix(ee_frame[:3, 0], thetas))
-        J = sp.lambdify([thetas, target], j_position.row_insert(4, j_orientation))
-
-        dist_arr, vectors_arr = [], []
-
-        dist, unit_dist, error = self.compute_error(qi, target)
-        dist_arr.append(dist)
-        vectors_arr.append(unit_dist)
-        while dist > max_dist or unit_dist > 10 ** (-5):
-            Jinv = inverse_jacobian(self.j_matrix, qi)
-            qi = (qi + dt * Jinv * kp * error).evalf()
-            dist, unit_dist, error = self.compute_error(qi, target)
-
-            print(f"{dist = }, {unit_dist = }, {len(vectors_arr) = }", end='\r')
-            dist_arr.append(dist)
-            vectors_arr.append(unit_dist)
-
-        qi = [q for q in qi]
-
-        print()
-        new_q_sol_arr = []
-        for a in qi:
-            a = a % (2 * sp.pi)
-            if a > sp.pi:
-                a -= 2 * sp.pi
-            new_q_sol_arr.append(sp.deg(a).evalf())
-        return new_q_sol_arr
-
-    def compute_error(self, thetas, target):
-        vect = sp.Matrix(target[:3]) - sp.Matrix(self.forward_kinematics(thetas)[:3])
-        angle = sp.Matrix([self.orientation(thetas, target)])
-        error = vect.row_insert(4, angle)
-        dist = sp.sqrt(vect.dot(vect)).evalf()
-        unit_dist = np.abs(angle[0])
-        return dist, unit_dist, error
--- src/matrix_utils.py
+++ src/matrix_utils.py
@@ -2,18 +2,20 @@ import sympy as sp
 import numpy as np
 
 
-def ee_transformation(thetas, dh_table, base_frame=sp.eye(4)):
-    for alpha, length, offset, theta in zip(dh_table['alpha'], dh_table['length'],
-                                            dh_table['offset'], thetas):
-        a = transformation_matrix(sp.rad(alpha), length, theta, offset)
-        base_frame = base_frame * a
-    return base_frame
+def ee_transformation(angles, dh_table, frame=sp.eye(4)):
+    for alpha, length, offset, theta, angle in zip(dh_table['alpha'], dh_table['length'],
+                                                   dh_table['offset'], dh_table['theta'],
+                                                   angles):
+        a = transformation_matrix(sp.rad(alpha), length, sp.rad(theta) + angle, offset)
+        frame = frame * a
+    position = frame[:3, 3]
+    orientation = rot2eul(frame[:3, :3])
+    return position.col_join(orientation)
 
 
 def target_transformation(target):
     px, py, pz, alpha, beta, gamma = target
-    # return eul2rot(alpha, beta, gamma) * translation_matrix(px, py, pz)
-    return translation_matrix(px, py, pz) * eul2rot(alpha, beta, gamma)
+    return eul2rot(alpha, beta, gamma) * Txyz(px, py, pz)
 
 
 def transformation_matrix(alpha, length, theta, offset):
@@ -28,8 +30,7 @@ def transformation_matrix(alpha, length, theta, offset):
 
 
 def eul2rot(alpha, beta, gamma):
-    # return Rz(alpha) * Ry(beta) * Rz(gamma)
-    return Rz(alpha)
+    return Rz(alpha) * Ry(beta) * Rz(gamma)
 
 
 def rot2eul(matrix):
@@ -39,38 +40,31 @@ def rot2eul(matrix):
     return sp.Matrix([alpha, beta, gamma])
 
 
-def jacobian_matrix(matrix, thetas):
-    jacobian = []
-    for cell in matrix:
-        j = [sp.diff(cell, theta) for theta in thetas]
-        jacobian.append(j)
-    return sp.Matrix(jacobian)
+def Rx(q):
+    return sp.Matrix([[1, 0, 0, 0],
+                      [0, sp.cos(q), -sp.sin(q), 0],
+                      [0, sp.sin(q), sp.cos(q), 0],
+                      [0, 0, 0, 1]])
 
-def inverse_jacobian(J, thetas):
-    return sp.Matrix(np.linalg.pinv(J(thetas)))
 
-def translation_matrix(px, py, pz):
+def Ry(q):
+    return sp.Matrix([[sp.cos(q), 0, sp.sin(q), 0],
+                      [0, 1, 0, 0],
+                      [-sp.sin(q), 0, sp.cos(q), 0],
+                      [0, 0, 0, 1]])
+
+
+def Rz(q):
+    return sp.Matrix([[sp.cos(q), -sp.sin(q), 0, 0],
+                      [sp.sin(q), sp.cos(q), 0, 0],
+                      [0, 0, 1, 0],
+                      [0, 0, 0, 1]])
+
+
+def Txyz(px, py, pz):
     return sp.Matrix([
         [1, 0, 0, px],
         [0, 1, 0, py],
         [0, 0, 1, pz],
-        [0, 0, 0, 1]
-    ])
-
-
-def Ry(q):
-    return sp.Matrix([
-        [sp.cos(q), 0, sp.sin(q), 0],
-        [0, 1, 0, 0],
-        [-sp.sin(q), 0, sp.cos(q), 0],
-        [0, 0, 0, 1]
-    ])
-
-
-def Rz(q):
-    return sp.Matrix([
-        [sp.cos(q), -sp.sin(q), 0, 0],
-        [sp.sin(q), sp.cos(q), 0, 0],
-        [0, 0, 1, 0],
         [0, 0, 0, 1]
     ])
